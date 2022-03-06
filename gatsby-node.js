dd8021c64fa834727891ebcc85e3f5e7ab5f2b66
karol <karol@hpcnt.com>
2022-03-07 00:27:32 +0900

    1 complete

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,21 @@
+const path = require('path');
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {};
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+      },
+    },
+  });
+};
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
@@ -7,3 +25,4 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
+
